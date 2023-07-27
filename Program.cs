0187a49d6153a7c0e5de9870cdf73dbe422035f6
Vlad <miass28@mail.ru>
2023-07-27 16:26:16 +0500

    добавил программу

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = new string[] {"Hello", "2", "world", ":-)"};
+//string[] array = new string[] {"1234", "1567", "-2", "computer science"};
+//string[] array = new string[] {"Russia", "Denmark", "Kazan"};
+string[] res = new string[5];
+int j = 0;
+for(int i = 0;i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        res[j] = array[i];
+        Console.Write(res[j] + "  ");
+        j = j + 1;
+    }
+}
